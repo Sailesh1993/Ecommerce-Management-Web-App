87e956f555a174644b398e4b6423ebee335d99fb
Sailesh1993 <karki.saileshnep@gmail.com>
2023-09-03 22:55:12 +0300

    created userReducer and added CRUD operation

--- frontend/src/redux/reducers/usersReducer.ts
+++ frontend/src/redux/reducers/usersReducer.ts
@@ -1,34 +1,198 @@
-import { createAsyncThunk } from "@reduxjs/toolkit"
-import { User } from "../../types/User"
-import axios from "axios"
+import { PayloadAction, createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { NewUser, User, UserCredential, UserUpdate } from "../../types/User";
+import axios, { Axios, AxiosError } from "axios";
 
 interface UserReducer {
-    users : User[]
-    currentUser: User
-    loading: boolean
-    error: string
+  users: User[];
+  currentUser?: User;
+  loading: boolean;
+  error: string;
 }
 
 const initialState: UserReducer = {
-  users:[],
+  users: [],
   loading: false,
-  error: ""
-}
+  error: "",
+};
 
 interface FetchQuery {
-  page: number
-  perPage: number
+  page: number;
+  perPage: number;
 }
 
-export const fetchAllUser = createAsyncThunk(
+export const fetchAllUsers = createAsyncThunk(
   "fetchAllUsers",
-  async({
-    page,perPage
-  }: FetchQuery) => {
-   try {
-    const result = await axios.get<User[]>(`https://saileshecom-app.azurewebsites.net/api/v1/users`)
-   } catch (error) {
-    
-   }
+  async ({ page, perPage }: FetchQuery) => {
+    try {
+      const result = await axios.get<User[]>(
+        `https://saileshecom-app.azurewebsites.net/api/v1/users`
+      );
+      return result.data;
+    } catch (e) {
+      const error = e as AxiosError;
+      return error;
+    }
+  }
+);
+
+export const authenticate = createAsyncThunk(
+  "authentication",
+  async (access_token: string) => {
+    try {
+      const authentication = await axios.get<User>(
+        "https://saileshecom-app.azurewebsites.net/api/v1/auth/login",
+        {
+          headers: {
+            Authorization: `Bearer ${access_token}`,
+          },
+        }
+      );
+      console.log(authentication.data);
+      return authentication.data;
+    } catch (e) {
+      const error = e as AxiosError;
+      return error;
+    }
   }
 )
+
+export const login = createAsyncThunk(
+  "login",
+  async ({ email, password }: UserCredential, { dispatch }) => {
+    try {
+      const result = await axios.post<{ access_token: string }>(
+        "https://saileshecom-app.azurewebsites.net/api/v1/auth/login/",
+        { email, password }
+      );
+      localStorage.setItem("token", result.data.access_token);
+      const authentication = await dispatch(
+        authenticate(result.data.access_token)
+      );
+      return authentication.payload as User;
+    } catch (e) {
+      const error = e as AxiosError;
+      return error;
+    }
+  }
+)
+
+export const createNewUser = createAsyncThunk(
+  "createNewUser", 
+  async (user: NewUser) => {
+    try {
+      const result = await axios.post<User>(
+        "https://saileshecom-app.azurewebsites.net/api/v1/users/", user)
+      console.log(result.data)
+      return result.data
+    } catch (e) {
+        const error = e as AxiosError
+        throw new Error(error.message)
+    }
+  }
+)
+
+const userSlice = createSlice({
+  name: "users",
+  initialState,
+  reducers: {
+    createUser: (state, action: PayloadAction<User>) => {
+      state.users.push(action.payload)
+    },
+    updateUserReducer: (state, action: PayloadAction<User[]>) => {
+
+    },
+    emptyUserReducer: (state) => {
+      state.users = []
+    },
+    updateOneUser: (state, action: PayloadAction<UserUpdate>) => {
+      const users = state.users.map(user => {
+        if(user.id === action.payload.id) {
+          return { ...user, ...action.payload.update}
+        }
+        return user
+      })
+      return {
+        ...state,
+        users
+      }
+    },
+    sortByEmail: (state, action: PayloadAction<"asc" | "desc">) => {
+      if(action.payload === "asc") {
+        state.users.sort((a,b) => a.email.localeCompare(b.email))
+      }
+      else {
+        state.users.sort((a,b) => b.email.localeCompare(a.email))
+      }
+    },
+  },
+  extraReducers: (build) => {
+    build
+      .addCase(fetchAllUsers.fulfilled, (state, action) => {
+        if(action.payload instanceof AxiosError) {
+          state.error = action.payload.message
+        }
+        else{
+          state.users = action.payload
+        }
+        state.loading = false
+      })
+      .addCase(fetchAllUsers.pending, (state, action) => {
+        state.loading = true
+      })
+      .addCase(fetchAllUsers.rejected, (state, action) => {
+        state.error = "Cannot fetch data"
+      })
+      .addCase(createNewUser.fulfilled, (state, action) => {
+        state.loading = false
+        state.users.push(action.payload)
+      })
+      .addCase(createNewUser.pending, (state, action) => {
+        state.loading = true
+      })
+      .addCase(createNewUser.rejected, (state, action) => {
+        state.error = "Failed to create new user!" 
+      })
+      .addCase(login.fulfilled, (state, action) => {
+        if(action.payload instanceof AxiosError){
+          state.error = action.payload.message
+        }
+        else {
+          state.currentUser = action.payload
+          state.loading = false
+        }
+      })
+      .addCase(login.pending, (state, action) => {
+        state.loading = true
+      })
+      .addCase(login.rejected, (state, action) => {
+        state.error = "Failed login"
+      })
+      .addCase(authenticate.fulfilled, (state, action) => {
+        if(action.payload instanceof AxiosError) {
+          state.error = action.payload.message
+        }
+        else {
+          state.currentUser = action.payload
+          console.log(state.currentUser)
+        }
+        state.loading = false
+      })
+      .addCase(authenticate.pending, (state, action) => {
+        state.loading = true
+      })
+      .addCase(authenticate.rejected, (state, action) => {
+        state.error = "Failed authentication"
+      })
+  }
+})
+
+const usersReducer = userSlice.reducer
+export const 
+{
+  createUser,
+  updateUserReducer,
+  emptyUserReducer,
+  updateOneUser,
+  sortByEmail
+} = userSlice.actions
+export default usersReducer
